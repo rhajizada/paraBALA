be1043e5a1204f2fae21eb3f2d43e72cb5305604
Rufat Hajizada <vice.genius@gmail.com>
2016-01-07 03:55:17 +0400

    Initial Commit

---
+++ parabala/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  parabala
+//
+//  Created by Rufat Hajizada on 1/7/16.
+//  Copyright © 2016 Rufat Hajizada. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
